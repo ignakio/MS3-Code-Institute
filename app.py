bbea429cd6f97002d5c61bc235dba70bd9ecb88c
ignakio <nacho.cladera@hotmail.com>
2022-06-16 17:36:47 +0000

    Changed true to False

--- app.py
+++ app.py
@@ -355,5 +355,5 @@ def handle_404(app_error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
             
